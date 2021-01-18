20ee6501bf62941d4f02aeafede4b1dc3b31028e
Saaanyyyaa <56549942+Saaanyyyaa@users.noreply.github.com>
2021-01-18 12:26:50 +0300

    Add files via upload

---
+++ practice3/src/Square.java
@@ -0,0 +1,44 @@
+public class Square extends Rectangle {
+    protected double side;
+
+    public Square() {
+        this.filled = false;
+        this.color = "blue";
+        side = 7;
+    }
+
+    public Square(double side) {
+        this.filled = false;
+        this.color = "blue";
+        this.side = side;
+    }
+
+    public Square(double side, String color, boolean filled) {
+        this.side = side;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side*side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape: square, side: "+this.side+", color: "+this.color;
+    }
+}
